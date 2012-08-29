620c052e93989cf235bdc64573f435dcaf4b5c72
Hugo Vindas <hugovin@gmail.com>
2012-08-29 06:41:02 -0600

    Added Files to Handle Shopping Cart
    Added Files to Handle Shopping Cart so you will be a ble to add/remove
    items to the cart

--- ED_LearnPad_Core/Services/Cart.cs
+++ ED_LearnPad_Core/Services/Cart.cs
@@ -7,6 +7,9 @@ namespace ED_LearnPad_Core
 {
     public class Cart
     {
-
+         public bool AddItemToCart(int title_id, string sku, int quantity,int cartId)
+         {
+             
+         }
     }
 }
---
+++ Ed_LearnPad/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ed_LearnPad.Controllers
+{
+    public class CartController : Controller
+    {
+       
+        public JsonResult AddProduct(int title_id, string sku, int quantity)
+        {
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
